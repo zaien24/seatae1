187dbff554b11f5afb2687c35e2c61f4c98c5491
한성길 <zaien24@naver.com>
2025-03-30 17:41:06 +0900

    first commit

---
+++ src/main/java/com/tae/seatae1/Seatae1Application.java
@@ -0,0 +1,13 @@
+package com.tae.seatae1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seatae1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Seatae1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tae/seatae1/Seatae1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tae.seatae1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seatae1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
